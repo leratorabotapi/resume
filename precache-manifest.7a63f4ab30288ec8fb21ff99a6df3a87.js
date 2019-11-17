dda49d434158c98e0a5e39ecf05ad5a3d5cc12f5
lerato <leratorabotapi@hotmail.com>
2019-11-17 10:51:19 +0200

    Updates

---
+++ precache-manifest.7a63f4ab30288ec8fb21ff99a6df3a87.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcc92f97b63a137f7b6e1d6ddf26f6e6",
+    "url": "/resume/static/media/html-5.bcc92f97.png"
+  },
+  {
+    "revision": "ccdb6d84cb8c89449295",
+    "url": "/resume/static/css/main.263c4552.chunk.css"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.fd78f13f.js"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/resume/static/media/express1.98ff4822.png"
+  },
+  {
+    "revision": "06bffe1a4d20445c92d4",
+    "url": "/resume/static/js/2.06bffe1a.chunk.js"
+  },
+  {
+    "revision": "e9e98ca1c7ca72c93aa9a7c18051db30",
+    "url": "/resume/static/media/letter.e9e98ca1.png"
+  },
+  {
+    "revision": "640145eade146cb694fb61cd421d4035",
+    "url": "/resume/static/media/about.640145ea.jpg"
+  },
+  {
+    "revision": "ccdb6d84cb8c89449295",
+    "url": "/resume/static/js/main.ccdb6d84.chunk.js"
+  },
+  {
+    "revision": "2d1887dd02e7a0c4b309d18c6ad1f6a9",
+    "url": "/resume/static/media/css3.2d1887dd.png"
+  },
+  {
+    "revision": "28218e9cd013fb7ab08334d70d0ee72a",
+    "url": "/resume/static/media/react-native.28218e9c.png"
+  },
+  {
+    "revision": "b73453c62371ffa61b16dce4dd72ae65",
+    "url": "/resume/static/media/nodejspng.b73453c6.png"
+  },
+  {
+    "revision": "fc7127988adc772a36ab1af5f9b1a582",
+    "url": "/resume/static/media/jquery.fc712798.png"
+  },
+  {
+    "revision": "06bffe1a4d20445c92d4",
+    "url": "/resume/static/css/2.a75071b7.chunk.css"
+  },
+  {
+    "revision": "7afdd1692811c232097884fa320847bb",
+    "url": "/resume/index.html"
+  }
+];
